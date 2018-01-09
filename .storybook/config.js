c9401f15496486388900bd0520350a3ba54dd840
TimHyatt707 <timhyatt707@gmail.com>
2018-01-08 23:33:12 -0800

    added landing page story

--- .storybook/config.js
+++ .storybook/config.js
@@ -3,6 +3,7 @@ import { configure } from "@storybook/react";
 function loadStories() {
   require("../src/stories/landingPageStories/LandingPageHeader.story");
   require("../src/stories/landingPageStories/LandingPageSignupForm.story");
+  require("../src/stories/landingPageStories/LandingPage.story");
 }
 
 configure(loadStories, module);
